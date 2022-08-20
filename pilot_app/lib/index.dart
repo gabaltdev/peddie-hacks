ef8f922f67fcaff4e82fd9dba8b74d8801d0ed09
gabaltdev <edxe1al7@gmail.com>
2022-08-20 15:01:34 -0600

    I delete Home, edit Main and other files with bugs, also I create new files: Firestore.json, index, theme and util

---
+++ pilot_app/lib/index.dart
@@ -0,0 +1,2 @@
+// Export pages
+export 'screen1/screen1_widget.dart' show Screen1Widget;
--- pilot_app/lib/main.dart
+++ pilot_app/lib/main.dart
@@ -1,17 +1,55 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:holamundo/home.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'flutter_flow/flutter_flow_theme.dart';
+import 'flutter_flow/flutter_flow_util.dart';
+import 'flutter_flow/internationalization.dart';
+import 'index.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await FlutterFlowTheme.initialize();
+
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+
+  static _MyAppState of(BuildContext context) =>
+      context.findAncestorStateOfType<_MyAppState>()!;
+}
+
+class _MyAppState extends State<MyApp> {
+  Locale? _locale;
+  ThemeMode _themeMode = FlutterFlowTheme.themeMode;
+
+  void setLocale(String language) =>
+      setState(() => _locale = createLocale(language));
+  void setThemeMode(ThemeMode mode) => setState(() {
+        _themeMode = mode;
+        FlutterFlowTheme.saveThemeMode(mode);
+      });
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "Hola Mundo",
-        home: HelloWorldHome());
+      title: 'peddie-hacks',
+      localizationsDelegates: [
+        FFLocalizationsDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      locale: _locale,
+      supportedLocales: const [Locale('en', '')],
+      theme: ThemeData(brightness: Brightness.light),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: _themeMode,
+      home: Screen1Widget(),
+    );
   }
 }
